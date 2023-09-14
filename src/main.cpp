3bcf195ae16b9215e5503207b5cc3ca5460cae3c
poold3 <dallinpool@gmail.com>
2023-09-14 00:49:46 -0600

    Remove test outputs.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <filesystem>
 #include <stdexcept>
-#include <chrono>
 
 #include "PNG_Decoder.h"
 
@@ -16,8 +15,6 @@ int main(int argc, char * argv[]) {
     if (!std::filesystem::exists(pngPath)) {
       throw std::invalid_argument("'" + pngPath.string() + "' does not exist.");
     }
-
-    uint64_t start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
     PNG_Decoder decoder(pngPath);
     if (!decoder.IsOpen()) {
@@ -42,24 +39,6 @@ int main(int argc, char * argv[]) {
     if (unfilteredDataSize == 0) {
       throw std::runtime_error("Failed to allocate unfiltered data.");
     }
-
-    uint64_t end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-
-    //std::cout << (end - start) << std::endl;
-
-
-    std::cout << "const myCanvas = document.getElementById(\"myCanvas\");\nconst ctx = myCanvas.getContext(\"2d\");" << std::endl;
-    for (unsigned int i = 0; i < decoder.GetHeight(); ++i) {
-      for (unsigned int j = 0; j < decoder.GetWidth(); ++j) {
-        unsigned int r = static_cast<unsigned int>(*reinterpret_cast<unsigned char *>(unfilteredData + (i * decoder.GetWidth() * 4) + (j * 4)));
-        unsigned int g = static_cast<unsigned int>(*reinterpret_cast<unsigned char *>(unfilteredData + (i * decoder.GetWidth() * 4) + (j * 4) + 1));
-        unsigned int b = static_cast<unsigned int>(*reinterpret_cast<unsigned char *>(unfilteredData + (i * decoder.GetWidth() * 4) + (j * 4) + 2));
-        unsigned int a = static_cast<unsigned int>(*reinterpret_cast<unsigned char *>(unfilteredData + (i * decoder.GetWidth() * 4) + (j * 4) + 3));
-        std::cout << "ctx.fillStyle = \"rgba(" << r << "," << g << "," << b << "," << a << ")\";" << std::endl;
-        std::cout << "ctx.fillRect(" << j << "," << i << ", 1, 1);" << std::endl;
-      }
-    }
-
 
     std::free(compressedData);
     std::free(decompressedData);
